8ce42ef78fb2e962232591acf19ed8cef5ecc586
Takashi Mizohata <beatak@gmail.com>
2012-06-09 12:33:20 -0400

    organizing code

---
+++ assets/common.js
@@ -0,0 +1,29 @@
+(
+function() {
+  "use strict"
+
+  var $mon;
+  var $button;
+  var monitor = '<div id="monitorclear"><button>clear</button></div><div id="monitor"></div>';
+
+  var init = function () {
+    $(document.body).append(monitor);
+    $mon = $('#monitor');
+    $button = $('#monitorclear > button');
+    $button.click(onbuttonclick);
+    window.init();
+  };
+
+  var onbuttonclick = function (ev) {
+    $mon.html('');
+    ev.preventDefault();
+  };
+
+  window.logging = function (str) {
+    $mon.prepend('<div>' + str + '</div>');
+    return str;
+  };
+
+  $(init);
+}
+)();
--- assets/index.js
+++ assets/index.js
@@ -1,61 +1,45 @@
-(
-  function () {
-    var arr = ['#111', '#333', '#666', '#999', '#aaa', '#ccc', '#eee'];
+(function () {
+  "use strict"
+  var arr = ['#111', '#333', '#666', '#999', '#aaa', '#ccc', '#eee'];
 
-    var $mon;
-    var $button;
-
-    var init = function () {
-      $mon = $('#monitor');
-      $button = $('#monitorclear > button');
-      $button.click(
-        function (ev) {
-          $mon.html('');
-          ev.preventDefault();
-        }
-      );
-      $('.yaylist').scrolling(
-        {
-          scroll: onscroll,
-          appear: onappear,
-          disappear: ondisappear,
-          positionchange: onpositionchange
-        }
-      );
-    };
-
-    var onpositionchange = function (ev) {
-      // console.log(ev);
-      $mon.prepend('<div>position changed: ' + $(ev.target).data('num') + '</div>');
-    };
-
-    var onscroll = function (ev) {
-      // console.log(ev);
-      $mon.prepend('<div>' + JSON.stringify(ev.scrolling) + '</div>');
-    };
-
-    var onappear = function (ev) {
-      console.log('on appear: ', ev);
-      $mon.prepend('<div>appear: '+ $(ev.target).data('num') + '</div>');
-    }
-
-    var ondisappear = function (ev) {
-      console.log('on disappear: ', ev);
-      $mon.prepend('<div>disappear: ' + $(ev.target).data('num') + '</div>');
-    }
-
-    $(
-      function () {
-        console.log('hi');
-        var $mylist = $('.mylist');
-        var $li;
-        for (var i = 0, len = arr.length; i < len; ++i) {
-          $li = $('<li class="yaylist" data-num="' + i + '">' + 'my number: ' + i + '</li>');
-          $li.css('background-color', arr[i]);
-          $mylist.append($li);
-        }
-        setTimeout(init, 500);
+  window.init = function () {
+    var $mylist = $('.mylist');
+    $.each(
+      arr,
+      function (i, elm) {
+        var $li = $('<li class="yaylist" data-num="' + i + '">' + 'my number: ' + i + '</li>');
+        $li.css('background-color', elm);
+        $mylist.append($li);
       }
     );
+
+    $('.yaylist').scrolling(
+      {
+        scrolltick: onscrolltick,
+        appear: onappear,
+        disappear: ondisappear,
+        positionchange: onpositionchange
+      }
+    );
+  };
+
+  var onpositionchange = function (ev) {
+    // console.log(ev);
+    logging('position changed: ' + $(ev.target).data('num'));
+  };
+
+  var onscrolltick = function (ev) {
+    // console.log(ev);
+    logging(JSON.stringify(ev.scrolling));
+  };
+
+  var onappear = function (ev) {
+    // console.log('on appear: ', ev);
+    logging($(ev.target).data('num'));
   }
-)();
+
+  var ondisappear = function (ev) {
+    // console.log('on disappear: ', ev);
+    logging($(ev.target).data('num'));
+  }
+})();
